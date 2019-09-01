e9a440c9da2b328df52c81bfabf069e100793f1d
tenger <tenger@lookteas.com>
2019-09-01 10:41:27 +0800

    update

--- data/sysdata/cache_mobile.php
+++
@@ -1,6 +0,0 @@
-<?php
-//Discuz! cache file, DO NOT modify me!
-//Identify: 9eff0624655901f5177af132add65691
-
-$mobilecheck = '{"discuzversion":"X3.2","charset":"utf-8","version":"4","pluginversion":"1.4.7","oemversion":"0","regname":"register","qqconnect":"0","sitename":"Discuz! Board","mysiteid":"63361955","ucenterurl":"http:\\/\\/test2.plpkj.net\\/uc_server","setting":{"closeforumorderby":null},"extends":{"used":null,"lastupdate":null}}';
-?>
--- data/sysdata/cache_pluginsetting.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Discuz! cache file, DO NOT modify me!
-//Identify: 292548576e5e5352b61307bb2703f41d
-
-$pluginsetting = array (
-);
-?>
